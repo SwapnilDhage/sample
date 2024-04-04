f23841ad04dbaeb4cfeecfc42323fa7b0fce6646
swapnil.com <brock9011@gmail.com>
2024-04-04 23:59:59 +0530

    1st commit

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+x=int(input("Enter age"))
+total_age=90-x
+
+rem_days=total_age*365
+rem_month=total_age*12
+rem_year=total_age*1
+
+print(f"you have remain {rem_days}days \n {rem_month}month \n {rem_year} year")
