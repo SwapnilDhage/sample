2b3212ce3c7af7f2d4e1dd54981053d691b36160
swapnil.com <brock9011@gmail.com>
2024-04-05 14:45:32 +0530

    Add Another code

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,31 @@
-x=int(input("Enter age"))
-total_age=90-x
+class animal:
+    def __init__(self,name):
+        self.name = name
 
-rem_days=total_age*365
-rem_month=total_age*12
-rem_year=total_age*1
+    def display1(self):
+        return f"my animal name is {self.name}"
 
-print(f"you have remain {rem_days}days \n {rem_month}month \n {rem_year} year")
+
+
+class category(animal):
+    def __init__(self,name,species,):
+        super().__init__(name)
+        self.species = species
+
+    def display2(self):
+        return f"species is {self.species}"
+
+
+
+class cat(category):
+    def __init__(self,name,species,sound):
+        super().__init__(name,species)
+        self.sound = sound
+
+    def display(self):
+        return f"{self.display1()} {self.display2()} its sound like a {self.sound}"
+
+
+x =  cat("manimew","bagadbilla","mewmew")
+k = x.display()
+print(k)
