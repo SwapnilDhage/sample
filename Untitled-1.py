23f34da95d0d1ccac32a9ad0697bd9c592a1ba71
swapnil.com <brock9011@gmail.com>
2024-04-05 17:35:07 +0530

    add new code

---
+++ Untitled-1.py
@@ -0,0 +1,39 @@
+# class animal:
+#     def __init__(self,name):
+#         self.name = name
+
+#     def display1(self):
+#         return f"my animal name is {self.name}"
+
+
+
+# class category(animal):
+#     def __init__(self,name,species,):
+#         super().__init__(name)
+#         self.species = species
+
+#     def display2(self):
+#         return f"species is {self.species}"
+
+
+
+# class cat(category):
+#     def __init__(self,name,species,sound):
+#         super().__init__(name,species)
+#         self.sound = sound
+
+#     def display(self):
+#         return f"{self.display1()} {self.display2()} its sound like a {self.sound}"
+
+
+# x =  cat("manimew","bagadbilla","mewmew")
+# k = x.display()
+# print(k)
+
+
+
+
+
+a = "hello"
+b = a[::-1]
+print(b)
